2ed4583e62dd086695806510b8367836fcb13c85
João Rodrigues <joao_mondim_16@hotmail.com>
2022-11-27 19:45:42 +0000

    Final commit

--- tiqets/wsgi.py
+++ tiqets/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Tiqets project.
+WSGI config for tiqets project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tiqets.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tiqets.settings")
 
 application = get_wsgi_application()
